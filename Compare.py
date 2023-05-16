60f896d449c28c83e62f41e34be0113ecd207f99
Daniel Zhang <dkzhang@ad.unc.edu>
2023-05-16 10:27:03 -0400

    Changed to file path input

--- Compare.py
+++ Compare.py
@@ -4,29 +4,26 @@ import os
 import shutil
 
 
-def compare(folder, fp1, fp2):
+def compare(folder_path, fp1, fp2):
     # returns true if same, false if different. Takes in 2 file paths and maximum mse as arguments.
-    fn1 = './img/' + folder + '/' + fp1
-    fn2 = './img/' + folder + '/' + fp2
+    fn1 = folder_path + '/' + fp1
+    fn2 = folder_path + '/' + fp2
     if (not (fn1.lower().endswith(('.png', '.jpg', '.jpeg')) and fn2.lower().endswith(('.png', '.jpg', '.jpeg')))):
         return False
     try:
         image1 = Image.open(fn1)
         image2 = Image.open(fn2)
-        if (image1.size != image2.size):
-            return False
-        if (image1 == image2):
-            return True
+        if (image1.size == image2.size):
+            if (image1 == image2):
+                return True
         return False
-        #image1.close()
-        #image2.close()
     except:
         return False
     
 
-def find_duplicates(folder):
+def find_duplicates(folder_path):
     # creates 2d list where each element is another list of files that are the same image
-    file_list = os.listdir("./img/" + folder)
+    file_list = os.listdir(folder_path)
     dups = []
     dup_element = []
     i = 0
@@ -38,7 +35,7 @@ def find_duplicates(folder):
         while (j < len(file_list)):
             #print(i)
             fp2 = file_list[j]
-            if (compare(folder, fp1, fp2)):
+            if (compare(folder_path, fp1, fp2)):
                 if (len(dup_element) == 0):
                     dup_element.append(fp1)
                 dup_element.append(fp2)
@@ -55,11 +52,11 @@ def find_duplicates(folder):
         i += 1
     return dups
 
-def move_duplicates(folder, dups):
+def move_duplicates(folder_path, dups):
     # moves the file names in 2d list to another folder with subfolders for each element
     errfn = []
     if (len(dups) != 0):
-        MYDIRDUPS = './img/' + folder + '/' + "duplicates"
+        MYDIRDUPS = folder_path + '/' + "duplicates"
 
         if not os.path.isdir(MYDIRDUPS):
             os.makedirs(MYDIRDUPS)
@@ -79,7 +76,7 @@ def move_duplicates(folder, dups):
             else:
                 print(MYDIRDUP, "folder already exists.")  
             for o in dup:
-                src = './img/' + folder + '/' + o
+                src = folder_path + '/' + o
                 dst = MYDIRDUP + '/' + o
                 try:
                     shutil.move(src, dst)
@@ -90,7 +87,7 @@ def move_duplicates(folder, dups):
 
 
 if __name__ == "__main__":
-    folder = "Test"
+    folder = "./img/2020"
     dups = find_duplicates(folder)
     errfn = move_duplicates(folder, dups)
     if (len(errfn) != 0):
